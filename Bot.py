1c39208768c6b7080bf9a042a899cd11c3d24ab1
nathac12 <nathac12@uci.edu>
2025-07-31 18:18:56 -0700

    basic boot up message

--- Bot.py
+++ Bot.py
@@ -1,1 +1,14 @@
-hello
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '!')
+
+@client.event
+async def on_ready():
+    print("Bot Is Ready! Logged in as: {client.user}")
+
+@client.event
+async def hello(ctx):
+    await ctx.send("Hello, I am a bot.")
+
+client.run('MTQwMDYzMzQxNjQ0MTAwNDA5Mw.GAwTux.1RXAkPFAj-77Mp454DHJe7x_J3lnuPw0Di5J4A')
