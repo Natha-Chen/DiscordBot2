80706096e0d8eaeacb10e773d60dc3b3125f0b11
nathac12 <nathac12@uci.edu>
2025-07-31 18:44:29 -0700

    changes to bot appearance

--- Bot.py
+++ Bot.py
@@ -1,7 +1,11 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '!', intents=discord.Intents.default())
+intents = discord.Intents.default() 
+intents.message_content = True 
+intents.guilds = True
+
+client = commands.Bot(command_prefix = '!', intents=intents)
 
 @client.event
 async def on_ready():
@@ -9,6 +13,9 @@ async def on_ready():
 
 @client.command()
 async def hello(ctx):
+    print("hello")
     await ctx.send("Hello, I am a bot.")
 
-client.run('MTQwMDYzMzQxNjQ0MTAwNDA5Mw.GZ0AuI.WyjLsFsJUIH0w1WB7cZinQ4jjYEM3P_iNOKiq4')
+
+
+client.run('MTQwMDYzMzQxNjQ0MTAwNDA5Mw.Goyh5l.OeFKz44zcFPKati3-DCU0ceK7c6SVuEjZD6BGA')
