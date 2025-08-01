788aa88ef02c1603e3d9924e37acacd011b2fa51
nathac12 <nathac12@uci.edu>
2025-08-01 11:47:29 -0700

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -9,17 +9,25 @@ client = commands.Bot(command_prefix = '!', intents=intents)
 
 @client.event
 async def on_ready():
-    print("Bot Is Ready! Logged in as: {client.user}")
+    print("Bot Is Ready! Logged in as: " + client.user )
+
+@client.event
+async def on_message(message):
+    await client.process_commands(message)
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('will you marry me'):
+        await message.channel.send('⚡KYS⚡')
 
 @client.command()
 async def hello(ctx):
     print("hello")
-    await ctx.send("Hello, I am a bot.")
+    await ctx.channel.send("Hello, I am a bot.")
 
 @client.event
-async def on_message(message):
-    if message.content.startswith('will you marry me'):
-        channel = message.channel
-        await message.channel.send('⚡KYS⚡')
+async def on_member_join(member):
+    channel = client.get_channel('')
+    await channel.send("Hello")
 
 client.run('MTQwMDYzMzQxNjQ0MTAwNDA5Mw.Goyh5l.OeFKz44zcFPKati3-DCU0ceK7c6SVuEjZD6BGA')
